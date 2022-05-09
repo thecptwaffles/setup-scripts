54fdecb066017a5eb448fa6576234dffe9ec49ac
Cade <thecptwaffles@zohomail.com>
2022-05-08 20:30:10 -0600

    adding bashBasicInstaller.sh

---
+++ archBasicInstaller.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# update the system
+sudo pacman -Syu
+
+# Install zsh
+sudo pacman -Sy zsh
+
+#Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#git clone config files repo
+git clone https://github.com/thecptwaffles/dotFiles.git
+
+#move dotFiles to ~/.config/
+mv ~/dotFiles/coc/ ~/.config/
+mv ~/dotFiles/nvim/ ~/.config/
+mv ~/dotFiles/vifm/ ~/.config/
+mv ~/dotFiles/wslu/ ~/.config/
+mv ~/dotFiles/.zshrc ~
+
+#install open-jdk 17 and open-jre 17
+sudo pacman -Sy openjdk-17 openjre-17
+
+#install gimp minecraft steam discord and firefox
+sudo pacman -Sy gimp steam
+sudo pacman -Sy firefox
+sudo pacman -Sy discord
+
+
